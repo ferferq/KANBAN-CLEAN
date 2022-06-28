399035d2868645cc135c5ba8a829aaf4b781d92d
ferferq <fernandoalvesq@gmail.com>
2022-06-27 23:12:51 -0300

    fix: return to default in fist moment because i was a problem in production because i get of cdn react-dom

--- src/main/index.tsx
+++ src/main/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
-import ReactDom from 'react-dom/client';
+import ReactDom from 'react-dom';
 import { App } from '@/presentation/pages/app';
 
-const root = ReactDom.createRoot(document.getElementById('main'));
-root.render(<App />);
+ReactDom.render(<App />, document.getElementById('main'));
